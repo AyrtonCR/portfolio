edf77a609637bf32a91bf1ad201f8ef36ed799db
Ayrton Campbell <ayrton.campbell-reinheimer@developersinstitute.ac.nz>
2023-03-29 14:07:40 +1300

    reworked navbar almost complete

--- client/src/components/nav/upperNavBar.js
+++ client/src/components/nav/upperNavBar.js
@@ -1,48 +1,67 @@
-import style from "./upperNavBar.module.css";
+import style from "./upperNavBar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiFillCloseCircle } from "react-icons/ai";
-import { useState } from "react";
-import NavLinks from "./navLinks";
+import { useState, useEffect, useRef } from "react";
+import User from "../../utilities/images/celtic1.webp";
+import Portrait from "../../utilities/images/goat.png";
 
 const UpperNavBar = () => {
   const [open, setOpen] = useState(false);
 
-  const hamburgerIcon = (
-    <GiHamburgerMenu
-      className={style.Hamburger}
-      size="1.8rem"
-      onClick={() => setOpen(!open)}
-    />
-  );
+  let menuRef = useRef();
 
-  const closeIcon = (
-    <AiFillCloseCircle
-      className={style.Hamburger}
-      size="1.8rem"
-      onClick={() => setOpen(!open)}
-    />
-  );
-  const closeMobileMenu = () => setOpen(false);
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setOpen(false);
+        console.log(menuRef.current);
+      }
+    };
 
+    document.addEventListener("mousedown", handler);
+  });
   return (
-    <div className={style.upperNavContainer}>
-      <div className={style.upperNavItem}>
-        <a className={style.upperNavLink} href="www.google.com">
-          LinkedIn{" "}
-        </a>
-        <a className={style.upperNavLink} href="www.google.com">
-          CV{" "}
-        </a>
-        <nav className={style.MobileNavigation} href="www.google.com">
-          {" "}
-          {open ? closeIcon : hamburgerIcon}
-          {open && <NavLinks isMobile={true} closeMobileMenu />}
-        </nav>
+    <div className="upperNavContainer" ref={menuRef}>
+      <div
+        className="menuTrigger"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        <h4>OPEN</h4>
+      </div>
+      <div className="upperNavLinksContainer">
+        <div className={`dropdownMenu ${open ? "active" : "inactive"}`}>
+          <h3>
+            Look at this
+            <br />
+            <span>Stuff</span>
+          </h3>
+          <ul>
+            <DropdownItem img={User} text={"Local Surf Project"} />
+            <DropdownItem img={User} text={"Portfolio Project"} />
+            <DropdownItem img={User} text={"Other Project"} />
+            <DropdownItem img={User} text={"Another one"} />
+          </ul>
+        </div>
+        <div>
+          <h3>LinkedIn</h3>
+        </div>
+        <div>
+          <h3>LinkedIn</h3>
+        </div>
       </div>
     </div>
   );
 };
-
+function DropdownItem(props) {
+  return (
+    <li className="dropdownItem">
+      <img src={props.img}></img>
+      <a> {props.text}</a>
+    </li>
+  );
+}
 export default UpperNavBar;
 
 {
@@ -54,3 +73,10 @@ export default UpperNavBar;
   </Link>
 </nav>; */
 }
+
+// <a className={style.upperNavLink} href="www.google.com">
+// LinkedIn{" "}
+// </a>
+// <a className={style.upperNavLink} href="www.google.com">
+// CV{" "}
+// </a>
